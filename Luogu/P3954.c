6b343ce8e65c629e609e9cf2b866bcb4b9c96370
HelloW-D <yootme@hotmail.com>
2025-09-14 10:20:56 +0800

    20250914001

--- Luogu/P3954.c
+++ Luogu/P3954.c
@@ -2,7 +2,7 @@
 int main()
 {
     int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    printf("%0.0f", a * 0.2 + b * 0.3 + c * 0.5);
+    scanf("%d%d%d", &a, &b, &c);                    //输入
+    printf("%0.0f", a * 0.2 + b * 0.3 + c * 0.5);   //输出
     return 0;
 }

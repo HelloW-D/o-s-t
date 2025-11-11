e0bec5e06d64564da7789d5282f6764edcb825fc
HelloW-D <yootme@hotmail.com>
2025-11-11 22:22:58 +0800

    1

--- Leetcode/4(slow).cpp
+++ Leetcode/4(slow).cpp
@@ -7,15 +7,9 @@ using namespace std;
 
 class Solution {
 public:
-    /**
-     * @brief 合并两个有序数组直到达到中位数位置
-     * @param nums1 第一个有序数组
-     * @param nums2 第二个有序数组
-     * @param n 两个数组的总长度
-     * @return vector<int> 合并后的数组，包含足够计算中位数的元素
-     */
-    vector<int> mergeArraysUntilMedian(vector<int>& nums1, vector<int>& nums2, int n) {
-        int l1 = 0, l2 = 0;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int l1, l2, n = nums1.size() + nums2.size();
+        l1 = l2 = 0;
         vector<int> ans;
         while (l1 + l2 <= n / 2 && l1 < nums1.size() && l2 < nums2.size())
         {
@@ -34,28 +28,11 @@ public:
             ans.push_back(nums2[l2++]);
         while (l1 < nums1.size())
             ans.push_back(nums1[l1++]);
-        return ans;
-    }
-
-    /**
-     * @brief 计算合并后数组的中位数
-     * @param mergedArray 合并后的有序数组
-     * @param totalLength 两个原始数组的总长度
-     * @return double 中位数值
-     */
-    double calculateMedian(vector<int>& mergedArray, int totalLength) {
-        if (totalLength % 2 == 0)
+        if ((nums1.size() + nums2.size()) % 2 == 0)
         {
-            return (double)(mergedArray[totalLength/2] + mergedArray[totalLength/2-1]) / 2.0;
-        } else {
-            return (double)(mergedArray[totalLength/2]);
-        }
-    }
-
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size() + nums2.size();
-        vector<int> mergedArray = mergeArraysUntilMedian(nums1, nums2, n);
-        return calculateMedian(mergedArray, n);
+            return (double)(ans[n/2]+ans[n/2-1]) / 2.0;
+        }else
+            return (double)(ans[n/2]);
     }
 };
 
